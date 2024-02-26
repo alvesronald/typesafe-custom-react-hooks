950716e0c3c13c98d2ddf2923b8efeb6dd847605
Ronald Alves <92890340+alvesronald@users.noreply.github.com>
2024-02-26 08:13:44 -0300

    useIsMounted

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { useNumber } from './hooks/01-useNumber';
+import useIsMounted from './hooks/02-useIsMounted';
 
 function App() {
 
   const [ number, setNumber ] = useNumber(12)
+
+  const isMoutend = useIsMounted();
+
+
+  useEffect(() => {
+    if(isMoutend){
+      console.log('Component montado')
+    }
+  }, [isMoutend])
 
   return (
     <div className="App">
