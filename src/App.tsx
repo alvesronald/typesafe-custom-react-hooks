9bf48857560dee3a39b4adb1eeb35948e8b15ebb
Ronald Alves <92890340+alvesronald@users.noreply.github.com>
2024-02-27 08:32:00 -0300

    usePreviousValue

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
-import React, { useEffect } from 'react';
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
 import { useNumber } from './hooks/01-useNumber';
 import useIsMounted from './hooks/02-useIsMounted';
 import useDebouncedValue from './hooks/04-useDebouncedValue';
+import { usePreviousValue } from './hooks/05-usePreviousValue';
 
 function App() {
-
-  const [ number, setNumber ] = useNumber(12)
-
-  const debouncedValue = useDebouncedValue(number)
-
+  const [number, setNumber] = useNumber(12)
+  // const debouncedValue = useDebouncedValue(number, 4000)
   const isMoutend = useIsMounted();
-
+  const previousValue = usePreviousValue(number);
 
   useEffect(() => {
     if(isMoutend){
@@ -22,13 +19,15 @@ function App() {
 
   return (
     <div className="App">
-     <button onClick={() => setNumber(prevState => prevState + 1)}>Number</button>
-     <p>
-      DebouncedValue -  {debouncedValue}
-     </p>
-     <p>
-      Number -  {number}
-     </p>
+      <button onClick={() => setNumber(prevState => prevState + 1)}>
+        Number
+      </button>
+      <p>
+      previousValue -  {previousValue}
+      </p>
+      <p>
+        RealtimeValue -  {number}
+      </p>
     </div>
   );
 }
---
+++ src/hooks/05-usePreviousValue.ts
@@ -0,0 +1,8 @@
+import { useRef } from "react"
+
+
+export const usePreviousValue = <T>(value: T) => {
+    const previousValue = useRef(value)
+
+    return previousValue.current
+}
