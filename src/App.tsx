e81f64db3445a2dcf986cd433285f9d00d965fde
Ronald Alves <92890340+alvesronald@users.noreply.github.com>
2024-02-27 08:08:14 -0300

    useDebouncedValue

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import { useNumber } from './hooks/01-useNumber';
 import useIsMounted from './hooks/02-useIsMounted';
+import useDebouncedValue from './hooks/04-useDebouncedValue';
 
 function App() {
 
   const [ number, setNumber ] = useNumber(12)
+
+  const debouncedValue = useDebouncedValue(number)
 
   const isMoutend = useIsMounted();
 
@@ -19,7 +22,13 @@ function App() {
 
   return (
     <div className="App">
-     <button onClick={() => setNumber(1)}>Number</button>
+     <button onClick={() => setNumber(prevState => prevState + 1)}>Number</button>
+     <p>
+      DebouncedValue -  {debouncedValue}
+     </p>
+     <p>
+      Number -  {number}
+     </p>
     </div>
   );
 }
